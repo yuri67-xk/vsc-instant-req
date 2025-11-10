59cc7843659f0250d64666b4a371244b40e0da9b
yuri-qa67 <yuri.kamo@tangerine.io>
2025-11-10 20:30:13 +0900

    fix: ESLint設定を追加し、コードのリンティングエラーを修正
    - .eslintrc.jsを追加
    - security.ymlのTruffleHog設定を修正
    - InstantReqViewProvider.tsのESLintエラーを修正

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // Add custom rules here
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  ignorePatterns: [
+    'out/',
+    'node_modules/',
+    '*.js',
+    'src/webview/',
+  ],
+};
